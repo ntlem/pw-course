ba66345c1eeaade81c0f05de873a2ac3c602d8e4
K9 - Truong Lam <truonglam1783@gmail.com>
2024-09-24 19:12:25 -0400

    chore: sua loi vong lap

--- tests/lessson-5/test3.spec.ts
+++ tests/lessson-5/test3.spec.ts
@@ -11,7 +11,7 @@ test('Add todo', async ({ page }) => {
     });
 
     await test.step("Thêm mới 100 todo item có nội dung “Todo <i>”", async () => {
-        for (let i = 1; i <= 5; i++) {
+        for (let i = 1; i <= 100; i++) {
             await page.locator('//input[@id="new-task"]').fill(`Todo ${i}`);
             await page.locator('//button[@id="add-task"]').click();
         }
@@ -22,7 +22,7 @@ test('Add todo', async ({ page }) => {
             await dialog.accept();
         });
 
-        for (let i = 1; i <= 5; i++) {
+        for (let i = 1; i <= 100; i++) {
             if (i % 2 !== 0) {
                 await page.locator(`//button[@id="todo-${i}-delete"]`).click();
             }
